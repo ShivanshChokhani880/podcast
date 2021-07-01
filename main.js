0d2119364304f8a364bd656491180cdca68b4354
ShivanshChokhani880 <80053639+ShivanshChokhani880@users.noreply.github.com>
2021-07-01 20:39:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function business(){
+    window.location = "https://www.entrepreneur.com/article/240202";
+}
+function popular(){
+    window.location = "https://www.goodhousekeeping.com/life/entertainment/g28353940/best-podcasts/";
+}
+function fashion(){
+    window.location = "https://www.harpersbazaar.com/uk/fashion/fashion-news/g32299944/best-fashion-podcasts/";
+}
+function music(){
+    window.location = "https://www.marieclaire.com/culture/g31251700/music-podcasts/";
+}
+function sport(){
+    window.location = "https://www.bbc.co.uk/podcasts/category/sport";
+}
+function health(){
+    window.location = "https://www.healthline.com/health/best-health-podcast";
+}
